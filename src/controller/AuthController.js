b5a024dfac17adca3fa217c7bf1edcb1c6c498c4
eliedson <eliedson@Mrxv3n0M.MrxV3n0M>
2025-07-22 20:06:04 -0300

    Resolve merge conflict

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -61,7 +61,7 @@ const logoutUser = (req, res) => {
             return res.status(500).json({ message: 'Falha ao sair.' });
         }
         res.clearCookie('connect.sid'); // Limpar o cookie da sessão
-        return res.status(200).json({ message: 'Login bem-sucedido!' });
+        return res.status(200).json({ message: 'Desconectado com sucesso!' });
     });
 };
 
